fc0c8503a5beb0fd49e88afa14e7bebcadd197c2
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-04 21:37:24 +0200

    Testing workflow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Gbot -- PI
+                    Gbot &PI
                 </div>
 
                 <div class="links">
