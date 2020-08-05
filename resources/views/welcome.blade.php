1eac422d6da0256ca79496ac0ba21126ee58e181
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-05 20:33:20 +0200

    Hello From Python

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Gbot &PI
+                    Gbot &Pi;
                 </div>
 
                 <div class="links">
