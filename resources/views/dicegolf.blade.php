ee64fae6627cb722f81e253538789c15e723f292
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-10 20:56:20 +0200

    Hello From Python

--- resources/views/dicegolf.blade.php
+++ resources/views/dicegolf.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="content">
+    <div class="ribbon-content">
         <h1 class="ribbon">Just a Rounded Ribbon</h1>
         <h5>Responsive? Resize the window.</h5>
         <div id="dg-stats"></div>
