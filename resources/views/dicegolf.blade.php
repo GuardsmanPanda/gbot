6b04763960ad60a6c491438b3bca7e93376e1c47
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-11 19:29:13 +0200

    Hello From Python

--- resources/views/dicegolf.blade.php
+++ resources/views/dicegolf.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="ribbon-content">
         <h1 class="ribbon">DiceGolf-v2 Stats</h1>
-        <h5 class="mt-2">Sort By Most Throws or Highest Sum! {{$p_max->ms}}</h5>
+        <h5 class="mt-2">Sort By Most Throws or Highest Sum!</h5>
         <div id="dg-stats"></div>
     </div>
 
@@ -20,12 +20,20 @@
                 {title:"Twitch Name", field:"name", headerSort:false},
                 {title:"Length", field:"length", sorter:"number", formatter:"progress", formatterParams:{
                         min:0,
-                        max:10,
-                        color:"lightseagreen",
+                        max:{{$p_max->ml}},
+                        color:"burlywood",
+                        legend:true,
                         legendColor:"#000000",
                         legendAlign:"center",
                     }},
-                {title:"Total", field:"sum", sorter:"number"},
+                {title:"Total", field:"sum", sorter:"number", formatter:"progress", formatterParams:{
+                        min:0,
+                        max:{{$p_max->ms}},
+                        color:"burlywood",
+                        legend:true,
+                        legendColor:"#000000",
+                        legendAlign:"center",
+                    }},
                 {title:"Game", field:"game"},
                 {title:"Played At", field:"created_at", formatter:"datetime"}
             ],
