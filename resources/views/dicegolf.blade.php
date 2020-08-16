5446fb1fc8cfa7f1cef048c5f12ea15a632370d6
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-16 14:51:56 +0200

    Dicegolf-V3

--- resources/views/dicegolf.blade.php
+++ resources/views/dicegolf.blade.php
@@ -11,34 +11,44 @@
     <script>
         const table = new Tabulator("#dg-stats", {
             ajaxURL: "/dicegolf/stats?game=100",
+            ajaxFiltering: true,
+            initialHeaderFilter:[
+                {field:"game", value:"100"}
+            ],
             ajaxSorting: true,
-            height: "75vh",
             initialSort: [
                 {column:"length", dir:"desc"},
             ],
+            height: "75vh",
             layout: "fitData",
             columns:[
-                {title:"Rank", field:"rank", formatter:"rownum", headerSort:false},
-                {title:"Twitch Name", field:"name", headerSort:false},
-                {title:"Length", field:"length", width:210, sorter:"numeric", headerSortStartingDir:"desc",
-                    formatter:"progress", formatterParams:{
-                        min:0,
-                        max:{{$p_max->ml}},
-                        color:"lightGreen",
-                        legend:true,
-                        legendColor:"#000000",
-                        legendAlign:"center",
-                    }},
-                {title:"Total", field:"sum", width:210, sorter:"numeric", headerSortStartingDir:"desc",
-                    formatter:"progress", formatterParams:{
-                        min:0,
-                        max:{{$p_max->ms}},
-                        color:"lightGreen",
-                        legend:true,
-                        legendColor:"#000000",
-                        legendAlign:"center",
-                    }},
-                {title:"Game", field:"game"},
+                {title:"Stats, Click To Sort By Length or Sum",
+                    columns: [
+                        {title:"Rank", field:"rank", formatter:"rownum", headerSort:false},
+                        {title:"Twitch Name", field:"name", headerSort:false},
+                        {title:"Length", field:"length", width:210, sorter:"number", headerSortStartingDir:"desc",
+                            formatter:"progress", formatterParams:{
+                                min:0,
+                                max:{{$p_max->ml}},
+                                color:"lightGreen",
+                                legend:true,
+                                legendColor:"#000000",
+                                legendAlign:"center",
+                            }
+                        },
+                        {title:"Total", field:"sum", width:210, sorter:"number", headerSortStartingDir:"desc",
+                            formatter:"progress", formatterParams:{
+                                min:0,
+                                max:{{$p_max->ms}},
+                                color:"lightGreen",
+                                legend:true,
+                                legendColor:"#000000",
+                                legendAlign:"center",
+                            }
+                        },
+                    ]
+                },
+                {title:"Game", field:"game", headerSort:false, headerFilter:"number"},
                 {title:"Played At", field:"created_at", formatter:"datetimediff", formatterParams: {
                     humanize:true,
                     suffix: true
