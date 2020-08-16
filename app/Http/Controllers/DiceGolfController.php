d46162f7bf699b0ecc21da58987515e17ef56db3
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-16 21:00:10 +0200

    Dicegolf query param

--- app/Http/Controllers/DiceGolfController.php
+++ app/Http/Controllers/DiceGolfController.php
@@ -9,24 +9,23 @@ use Ramsey\Uuid\Type\Integer;
 
 class DiceGolfController extends Controller {
 
-    public function index() {
-        $p_max = DB::select("SELECT MAX(length) AS ml, MAX(sum) AS ms FROM dicegolf")[0];
-        return view('dicegolf', compact('p_max'));
+    public function index(Request $r) {
+        $start = intval($r->get('start') ?? 100);
+        $start = min($start, 2147483647);
+        $start = max($start, 2);
+        $p_max = DB::select(" SELECT MAX(length) AS ml, MAX(sum) AS ms FROM dicegolf WHERE start = ?", [$start])[0];
+        return view('dicegolf', compact('p_max', 'start'));
     }
 
     public function stats_gather(Request $r) {
-        $start = intval($r->get('filters')[0]['value'] ?? 100);
-        $start = min($start, 2147483647);
-        $start = max($start, 2);
         return DB::select("
             SELECT
                 t.name,
-                ROW_NUMBER() OVER (ORDER BY d.length DESC, d.sum DESC) AS rank,
                 d.*
             FROM dicegolf AS d
             LEFT JOIN tuis AS t ON d.tui = t.id
             WHERE d.start = ?
             LIMIT 100
-            ", [$start]);
+            ", [$r->get('start')]);
     }
 }
--- resources/views/dicegolf.blade.php
+++ resources/views/dicegolf.blade.php
@@ -10,49 +10,40 @@
 
     <script>
         const table = new Tabulator("#dg-stats", {
-            ajaxURL: "/dicegolf/stats",
-            ajaxFiltering: true,
-            initialHeaderFilter:[
-                {field:"game", value:"100"}
-            ],
+            ajaxURL: "/dicegolf/stats?start={{$start}}",
             ajaxSorting: true,
+            height: "75vh",
             initialSort: [
                 {column:"length", dir:"desc"},
             ],
-            ajaxResponse:function(url, params, response){
-                let maxLen = 1, maxSum = 1;
-                response.forEach(row => {
-                    maxLen = Math.max(maxLen, row.length);
-                    maxSum = Math.max(maxSum, row.sum);
-                });
-                table.updateColumnDefinition("length", { formatter:"progress", formatterParams:{
-                        max:maxLen,
-                        color:"lightGreen",
-                        legend:true,
-                        legendColor:"#000000",
-                        legendAlign:"center",
-                    }});
-                table.updateColumnDefinition("sum", { formatter:"progress", formatterParams:{
-                        max:maxSum,
-                        color:"lightGreen",
-                        legend:true,
-                        legendColor:"#000000",
-                        legendAlign:"center",
-                    }});
-                return response; //return the response data to tabulator
-            },
-            height: "75vh",
             layout: "fitData",
             columns:[
                 {title:"Rank", field:"rank", formatter:"rownum", headerSort:false},
                 {title:"Twitch Name", field:"name", headerSort:false},
-                {title:"Length", field:"length", width:210, sorter:"number", headerSortStartingDir:"desc"},
-                {title:"Total", field:"sum", width:210, sorter:"number", headerSortStartingDir:"desc"},
-                {title:"Game", field:"game", headerSort:false, headerFilter:"number"},
+                {title:"Length", field:"length", width:210, sorter:"numeric", headerSortStartingDir:"desc",
+                    formatter:"progress", formatterParams:{
+                        min:0,
+                        max:{{$p_max->ml}},
+                        color:"lightGreen",
+                        legend:true,
+                        legendColor:"#000000",
+                        legendAlign:"center",
+                    }},
+                {title:"Total", field:"sum", width:210, sorter:"numeric", headerSortStartingDir:"desc",
+                    formatter:"progress", formatterParams:{
+                        min:0,
+                        max:{{$p_max->ms}},
+                        color:"lightGreen",
+                        legend:true,
+                        legendColor:"#000000",
+                        legendAlign:"center",
+                    }},
+                {title:"Game", field:"game"},
                 {title:"Played At", field:"created_at", formatter:"datetimediff", formatterParams: {
                         humanize:true,
-                    suffix: true
-                }}
+                        suffix: true
+                    }
+                }
             ],
         });
     </script>
