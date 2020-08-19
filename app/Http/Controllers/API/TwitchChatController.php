7cca3ba89cea06201776daa85bd8cd9192fe4feb
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-19 22:10:29 +0200

    Chat badges work

--- app/Http/Controllers/API/TwitchChatController.php
+++ app/Http/Controllers/API/TwitchChatController.php
@@ -14,7 +14,7 @@ class TwitchChatController extends Controller {
             SELECT tu.flag
             FROM twitch_name_to_tui AS t
             LEFT JOIN tuis AS tu ON tu.id = t.tui
-            WHERE t.twitch_name = ? ", [$name])[0] ?? 'none';
+            WHERE t.twitch_name = ? ", [$name])[0]->flag ?? 'none';
         return $flag;
     }
 }
