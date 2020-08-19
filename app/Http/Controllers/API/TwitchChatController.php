45566fa514adad77fb190584b0f463e69b8537a5
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-19 21:45:34 +0200

    Dicegolf query param

--- app/Http/Controllers/API/TwitchChatController.php
+++ app/Http/Controllers/API/TwitchChatController.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Type\Integer;
+
+class TwitchChatController extends Controller {
+
+    public function get_badge(string $name) {
+        return '1';
+    }
+}
