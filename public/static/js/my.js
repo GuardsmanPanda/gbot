fe24fa6f0488985bf70cf025c0bd197b66390b73
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-24 20:46:54 +0200

    Chat badges work

--- public/static/js/my.js
+++ public/static/js/my.js
@@ -4,6 +4,6 @@ const tabulatorUTCToLocal = function(value, data, type, params, component){
     //type - the type of mutation occurring  (data|edit)
     //params - the mutatorParams object from the column definition
     //component - when the "type" argument is "edit", this contains the cell component for the edited cell, otherwise it is the column component for the column
-    console.log(moment.parse(value))
+    console.log(moment.utc(value).local())
     return value; //return the new value for the cell data.
 }
