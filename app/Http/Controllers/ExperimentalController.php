7dcfb5fffe9808bdbf7925da6df9df2b6c949f7a
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-25 20:49:41 +0200

    login with twitch work

--- app/Http/Controllers/ExperimentalController.php
+++ app/Http/Controllers/ExperimentalController.php
@@ -15,7 +15,7 @@ class ExperimentalController extends Controller {
     public function login_twitch(Request $r) {
         try {
             $prefix = "https://id.twitch.tv/oauth2/authorize?client_id=q8q6jjiuc7f2ef04wmb7m653jd5ra8&redirect_uri=https://gman.bot/oauth/twitch";
-            $resp = Http::post($prefix . "&scope=" . $r->get('scope' . "&code=" . $r->get('code')))->json();
+            $resp = Http::post($prefix . "&client_secret=" . env('TWITCH_SECRET') . "&scope=" . $r->get('scope' . "&code=" . $r->get('code')))->json();
             return 'ok - expires in: ' . $resp['expires_in'];
         } catch (\Exception $e) {
             return 'Not ok';
