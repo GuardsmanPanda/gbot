3c987ca7137a608a3ebc34dcc378fab5d050b2d8
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-26 20:02:34 +0200

    login with twitch work

--- app/Http/Controllers/ExperimentalController.php
+++ app/Http/Controllers/ExperimentalController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class ExperimentalController extends Controller {
+    private $client_id = 'q8q6jjiuc7f2ef04wmb7m653jd5ra8'
+
 
     public function index() {
         return view("experimental");
@@ -15,7 +17,7 @@ class ExperimentalController extends Controller {
 
     public function login_twitch(Request $r) {
         try {
-            $url = "https://id.twitch.tv/oauth2/token?client_id=q8q6jjiuc7f2ef04wmb7m653jd5ra8";
+            $url = "https://id.twitch.tv/oauth2/token?client_id=" . $this->client_id;
             $url .= "&client_secret=" . config('app.twitch_secret');
             $url .= "&redirect_uri=https://gman.bot/oauth/twitch";
             $url .= "&scope=" . $r->get('scope');
@@ -29,7 +31,9 @@ class ExperimentalController extends Controller {
             }
 
             $token = $resp->json()['access_token'];
-            $resp = Http::withToken($token)->get("https://api.twitch.tv/helix/users");
+            $resp = Http::withToken($token)
+                ->withHeaders(['Client-ID' => $this->client_id])
+                ->get("https://api.twitch.tv/helix/users");
             if ($resp->failed()) {
                 Log::warning($resp->body());
                 return 'Getting Twitch ID Failed.';
