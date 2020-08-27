76a6e3a8582ea49313e5410a7814775741a618c2
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-27 22:13:23 +0200

    Flag maneuver

--- app/Http/Controllers/API/TwitchChatController.php
+++ app/Http/Controllers/API/TwitchChatController.php
@@ -16,6 +16,6 @@ class TwitchChatController extends Controller {
             LEFT JOIN tuis AS tu ON tu.id = t.tui
             WHERE t.twitch_name = ? ", [mb_strtolower($name)])[0]->flag ?? 'none';
 
-        return response()->file(public_path("img/flags/$flag.png"))->setMaxAge(0);
+        return response()->file(public_path("static/img/flags/$flag.png"))->setMaxAge(0);
     }
 }
