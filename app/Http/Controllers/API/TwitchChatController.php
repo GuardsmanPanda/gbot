0c43a4ad242130ab7a24b4b042247a334f57d7d3
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-28 23:47:23 +0200

    Flag maneuver

--- app/Http/Controllers/API/TwitchChatController.php
+++ app/Http/Controllers/API/TwitchChatController.php
@@ -10,23 +10,35 @@ use Ramsey\Uuid\Type\Integer;
 class TwitchChatController extends Controller {
 
     public function get_badge(string $name) {
-        $flag = DB::select("
-            SELECT tu.flag
+        $db_res = DB::select("
+            SELECT tu.flag, u.tui
             FROM twitch_name_to_tui AS t
             LEFT JOIN tuis AS tu ON tu.id = t.tui
-            WHERE t.twitch_name = ? ", [mb_strtolower($name)])[0]->flag ?? 'none';
+            LEFT JOIN users AS u ON u.tui = t.tui
+            WHERE t.twitch_name = ? ", [mb_strtolower($name)])[0];
 
-        // CREATE BADGE IMAGE
+
+        //CREATE BADGE IMAGE
         $img = imagecreatetruecolor(400, 84);
         imagesavealpha($img, true);
         $color = imagecolorallocatealpha($img, 0, 0, 0, 127);
         imagefill($img, 0, 0, $color);
 
-        //PUT FLAG ON BAD IMAGE
+        //ADD SECRET WEBSITE ICON
+        if ($db_res->tui) {
+            $site_icon = imagecreatefrompng(public_path("static/img/misc/site_chat_icon.png"));
+            imagecopy($img, $flag_image, 202, 0, 0, 0, 84, 84);
+        }
+
+
+        //PUT FLAG ON IMAGE
+        $flag = $db_res->flag ?? 'none';
         $flag_image = imagecreatefrompng(public_path("static/img/flags/$flag.png"));
         imagecopy($img, $flag_image, 286, 0, 0, 0, 114, 84);
-        imagepng($img, storage_path("cache/chat_badge/$name"));
 
+
+        //Store the file, then load the file and send it on its way.
+        imagepng($img, storage_path("cache/chat_badge/$name"));
         return response()->file(storage_path("cache/chat_badge/$name"));
     }
 }
