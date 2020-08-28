cff970c573296a687a9c055de7a51bae74b9fb0a
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-28 21:01:42 +0200

    Flag maneuver

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function tui() {
+    public function tui_data() {
         return $this->belongsTo('App\Models\Tui', 'id', 'tui');
     }
 }
