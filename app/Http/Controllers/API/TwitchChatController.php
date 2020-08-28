c4b47333a81749cde2befa9929c8ab9f845bb830
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-28 21:50:38 +0200

    Flag maneuver

--- app/Http/Controllers/API/TwitchChatController.php
+++ app/Http/Controllers/API/TwitchChatController.php
@@ -26,6 +26,6 @@ class TwitchChatController extends Controller {
         $flag_image = imagecreatefrompng(public_path("static/img/flags/$flag.png"));
         imagecopy($img, $flag_image, 286, 0, 0, 0, 114, 84);
 
-        return response(imagepng($img));
+        return response(imagepng($img))->header('Content-Type','image/png');
     }
 }
