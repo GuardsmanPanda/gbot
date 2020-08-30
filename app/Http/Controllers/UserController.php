d8f51d3c465a6598bef0e6bef3789ee4a4b3a31c
Bjørn Dons <guardsmanpanda@gmail.com>
2020-08-30 22:00:51 +0200

    Flag maneuver

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ class UserController extends Controller {
     public function flag_selector() {
         $flags = scandir(public_path("static/img/flags"));
         $flags = array_filter($flags, function ($flag) { return strlen($flag) > 4; });
+        $flags = array_map(function ($flag) { return substr($flag, 0, strlen($flag)-4); }, $flags);
         return view('flag_selector', compact('flags'));
     }
 }
