4c3a0bb63f7797b3f0c60c26a2c85e4bb8156800
Bjørn Dons <guardsmanpanda@gmail.com>
2020-09-14 20:19:33 +0200

    chat stats updates

--- app/Http/Controllers/ChatStatsController.php
+++ app/Http/Controllers/ChatStatsController.php
@@ -23,11 +23,18 @@ class ChatStatsController extends Controller {
     }
 
     public function stats(Request $r) {
+        switch ($r->get('sorters')[0]['field'] ?? '') {
+            case "active_hours": $order = "active_hours"; break;
+            case "updated_at": $order = "updated_at"; break;
+            case "idle_hours": $order = "idle_hours"; break;
+            case "bob_coins": $order = "bob_coins"; break;
+            default: $order = "chat_lines"; break;
+        }
         return DB::select("
             SELECT
                    name, flag, chat_lines, active_hours, idle_hours, bob_coins, updated_at
             FROM tuis
-            ORDER BY chat_lines DESC
+            ORDER BY $order DESC
             LIMIT 250
         ");
     }
