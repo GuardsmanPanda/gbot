43769378fe454be9d622bdfb606a175a1dd52287
Bjørn Dons <guardsmanpanda@gmail.com>
2021-01-01 23:19:44 +0100

    cleanup

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,7 +16,6 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
         ],
     ];
 
